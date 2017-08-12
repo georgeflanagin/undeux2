74b21ba95d7e71a38ad7fafc3c22e2bd4b3b03cb
George Flanagin <me@georgeflanagin.com>
2017-08-11 23:02:43 -0400

    Added the last of the options.

--- dedup.py
+++ dedup.py
@@ -106,6 +106,11 @@ def dedup_help() -> int:
         one common ancestor directory are penalized even more.
     Files are penalized if their contents exactly match another
         file.
+    
+    So if you have an ancient file, that is a duplicate of some other
+    file with the same name somewhere on the same mount point, and it 
+    is large and hasn't been accessed in a while, then its score
+    may approach 1.0
 
     Through the options below, you will have a lot of control over
     how dedup works. You should read through all of them before you
@@ -133,20 +138,21 @@ def dedup_help() -> int:
         directories that may have been created by different people
         at different times. By default, --ignore-filenames is *OFF*
 
-    --output {file-name} 
-        This is the directory where names of possibly dup files will 
-        be placed. The default is a directory named 'dedups' in the 
-        user's home directory. If the directory does not exist, dedup 
-        will attempt to create it.
-
-        In addition to the links themselves, dedup writes a CSV file
-        explaining how each file was scored. 
-
     --nice {int} 
         Keep in mind a terabyte of disc could hold one million files 
         at one megabyte each. You should be nice, and frankly, the program
         may run faster in nice mode. The default value is 20, which
         on Linux is as nice as you can be.
+
+    --output {directory-name} 
+        This is the directory where names of possibly dup files will 
+        be placed. The default is a directory named 'dedups' in the 
+        user's home directory. If the directory does not exist, dedup 
+        will attempt to create it. This directory is never examined
+        for duplicate files, or more correctly, any file in it is 
+        assumed to be unique and worth keeping. 
+
+        The output is a CSV file named dedup.YYYY-MM-DD-HH-MM.csv
 
     --quiet 
         I know what I am doing. Just let me know when you are finished. 
@@ -180,7 +186,15 @@ def dedup_main() -> int:
         description='Find probable duplicate files, and create links to them.'
         )
     parser.add_argument('-?', '--help', action='store_true')
-
+    parser.add_argument('--quiet', action='store_true')
+    parser.add_argument('--output', type=str, default='~/dedups')
+    parser.add_argument('--version', action='store_true')
+    parser.add_argument('--young-file', type=int, default=365)
+    parser.add_argument('--small-file', type=int, default=4096)
+    parser.add_argument('--nice', type=int, default=20)
+    parser.add_argument('--ignore-filenames', action='store_true')
+    parser.add_argument('--dir', type=str, action='append', default=None)
+    parser.add_argument('--home', type=str, default='~')
 
     pargs = parser.parse_args()
     if pargs.help: return dedup_help()
@@ -192,16 +206,6 @@ if __name__ == '__main__':
     sys.exit(dedup_main())
 else
     pass
-
- 
-
-    PARSER.add_argument('root', metavar='R', help='Dir to search.' )
-    PARSER.add_argument('-remove', action='store_true',
-                         help='Delete duplicate files.' )
-    ARGS = PARSER.parse_args()
- 
-    DUPS = find_duplicates( ARGS.root )
- 
     print '%d Duplicate files found.' % len(DUPS)
     for f in sorted(DUPS):
         if ARGS.remove == True:
@@ -210,8 +214,3 @@ else
         else:
             print '\t'+ f
 
-if __name__ == "__main__":
-    oed = build_dictionary(os.environ.get('PWD'))
-    
-else:
-    pass
