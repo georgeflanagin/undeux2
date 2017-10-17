d4963f931392d6c5787afa417e4d677fd001f295
George Flanagin <me@georgeflanagin.com>
2017-10-17 10:16:55 -0400

    Removed boilerplate that is now in gkflib/sqlitedb module.

--- dedup.py
+++ dedup.py
@@ -33,6 +33,7 @@ from os import walk, remove, stat
 from os.path import join as joinpath
  
 import gkflib as gkf
+import sqlitedb
 import fname
 
 schema = [
@@ -47,65 +48,9 @@ schema = [
     ]
 
 
-class DeDupDB(object):
-    def __init__(self, path_to_db:str, force_new_db:bool = False):
-        """
-        Does what it says. Opens the database if present, and
-        creates the database if it is MIA.
-        """
-        stmt = ""
-        self.db = None
-        self.cursor = None
-
-        db_file = fname.Fname(path_to_db)
-        gkf.tombstone("Database name will be " + str(db_file))
-        if force_new_db:
-            try:
-                gkf.tombstone("Attempting to remove old database.")
-                os.remove(str(db_file))
-            except OSError as e:
-                # no harm in deleting a file that does not exist.
-                gkf.tombstone("Nothing to remove")
-            else:
-                gkf.tombstone("Removed.")
-
-        if db_file:
-            gkf.tombstone("Attempting to open existing database " + str(db_file))
-            self.db = sqlite3.connect(str(db_file), timeout=30, isolation_level='DEFERRED')
-            self.cursor = self.db.cursor()
-            gkf.tombstone("Database opened: " + str(fname.Fname(db_file)))
-            return
-
-        try:
-            gkf.tombstone("Attempting to create database in " + str(db_file))
-            self.db = sqlite3.connect(str(db_file), timeout=30, isolation_level='DEFERRED')
-            gkf.tombstone("New ishtar.db created: " + str(db_file))
-
-            for stmt in schema: self.db.execute(stmt)
-
-        except sqlite3.OperationalError as e:
-            gkf.tombstone(str(e))
-            if stmt: gkf.tombstone(stmt)
-            exit(os.EX_CANTCREAT)
-
-        else:
-            self.cursor = self.db.cursor()
-            gkf.tombstone('cursor created.')
-
-        finally:
-            self._keys_on()
-            gkf.tombstone('foreign keys are on')
-
-
-    def _keys_off(self):
-        self.cursor.execute('pragma foreign_keys = 0')
-        self.cursor.execute('pragma synchronous = OFF')
-
-
-    def _keys_on(self):
-        self.cursor.execute('pragma foreign_keys = 1')
-        self.cursor.execute('pragma synchronous = FULL')
-
+class DeDupDB(sqlitedb.SQLiteDB):
+    def __init__(self, path_to_db:str, force_new_db:bool = False, extra_DDL:list=[]):
+        sqlitedb.SQLiteDB.__init__(self, path_to_db, force_new_db, extra_DLL)
 
     def add_file_details(self, fileinfo:list) -> bool:
         """
