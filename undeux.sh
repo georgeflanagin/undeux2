1d18457aa8682ef26f4a06ad37a39ec673ebf73d
georgeflanagin <milesdavis@billevans>
2025-10-21 06:05:03 -0400

    updated

--- undeux.sh
+++ undeux.sh
@@ -1,11 +1,6 @@
-export undir="$sw/undeux"
-export latestpython=$(ls -1 /usr/bin/python3.? | tail -1)
-
 function undeux
 {
-    command pushd $undir >/dev/null
-    $latestpython undeux.py $@
-    command popd >/dev/null
+    python undeux.py $@
 }
 
 
